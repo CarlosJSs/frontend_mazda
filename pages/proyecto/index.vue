54ce96043a09e2e7a95a3294bd959ea59e69cc9a
Carlos Eduardo <carlos.0577@outlook.com>
2025-12-02 10:23:19 -0600

    diseñando tabla

--- pages/proyecto/index.vue
+++ pages/proyecto/index.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    hola
+    Proyectos
+    <div class="tablaContainer">
+      <projectsTable />
+    </div>
   </div>
 </template>
 
-<script>
-definePageMeta({
-  layout: 'proyecto'
-})
+<script setup>
+  import projectsTable from '~/components/projectsTable.vue';
+
+  definePageMeta({
+    layout: 'proyecto'
+  })
 </script>
+
+<style scoped>
+.tablaContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
